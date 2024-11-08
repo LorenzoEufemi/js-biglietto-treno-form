c2a62817813809e781faef44d1d866eee3022032
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-11-08 18:52:34 +0100

    feat: struttura

---
+++ script.js
@@ -0,0 +1,48 @@
+// INPUT
+const usernameInput = document.getElementById("username");
+const ageInput = document.getElementById("age");
+const kmInput = document.getElementById("km");
+const btnElem = document.getElementById("btn-submit")
+const usernameElem = document.getElementById("username-card");
+const ageElem = document.getElementById("age-card");
+const kmElem = document.getElementById("km-card");
+const priceElem = document.getElementById("price-card");
+const cardElem = document.querySelector(".card");
+const formElem = document.getElementById("user-form")
+
+// ESECUZIONE
+
+
+
+formElem.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = usernameInput.value.trim();
+    const age = parseInt(ageInput.value.trim());
+    const km = parseInt(kmInput.value.trim());
+    console.log(typeof age, km)
+
+    usernameElem.innerHTML = username;
+    ageElem.innerHTML = age;
+    kmElem.innerHTML = km;
+
+    const fullPrice = km * 0.21;
+    console.log(fullPrice)
+    const price20 = fullPrice - (fullPrice * 0.2); // 0.2 = 20%
+    const price40 = fullPrice - (fullPrice * 0.4); // 0.4 = 40%
+
+    let result;
+
+    if (age < 18) {
+        result = price20.toFixed(2);
+    } else if (age >= 65) {
+        result = price40.toFixed(2);
+    } else {
+        result = fullPrice.toFixed(2);
+    }
+    console.log(result)
+    priceElem.innerHTML = result + "&#8364";
+    cardElem.classList.remove("d-none");
+    formElem.reset();
+
+}
+)
